59d8b6beb8e20692dad1fdcaa61b1e727031e03a
antoinegreuzard <antoine@antoinegreuzard.fr>
2024-02-27 21:29:25 +0100

    refactor: rename folder

---
+++ app/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from views import CreateUserView
+
+urlpatterns = [
+    path('api/signup/', CreateUserView.as_view(), name='signup'),
+]
--- server/urls.py
+++ server/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for monprojet project.
+URL configuration for server project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -25,4 +25,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('app/', include('app/urls'))
 ]
