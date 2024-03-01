d80fd8e885556ba12ca1ab087ed8688cee430666
antoinegreuzard <antoine@antoinegreuzard.fr>
2024-03-01 15:21:48 +0100

    refactor: optimize js

--- app/static/app/js/script.js
+++ app/static/app/js/script.js
@@ -1,19 +1,31 @@
-if (document.getElementById('searchInput')) {
-  document.getElementById('searchInput').addEventListener('input', function () {
-    const input = this.value;
-    const autocompleteList = document.getElementById('autocomplete-list');
+function debounce(func, wait) {
+  let timeout;
+  return function () {
+    const context = this, args = arguments;
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(context, args), wait);
+  };
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  const searchInput = document.getElementById('searchInput');
+  const autocompleteList = document.getElementById('autocomplete-list');
+  const searchForm = document.getElementById('searchForm');
+
+  if (!searchInput || !autocompleteList || !searchForm) return;
+
+  const fetchAutocompleteData = debounce(function (input) {
     if (!input) {
-      document.getElementById('autocomplete-list').innerHTML = '';
-      this.style.borderBottomLeftRadius = ''
-      this.style.borderBottomRightRadius = ''
-      autocompleteList.style.borderWidth = ''
+      autocompleteList.innerHTML = '';
+      searchInput.style.borderBottomLeftRadius = '';
+      searchInput.style.borderBottomRightRadius = '';
+      autocompleteList.style.borderWidth = '';
       return;
-    } else {
-      this.style.borderBottomLeftRadius = '0'
-      this.style.borderBottomRightRadius = '0'
-      autocompleteList.style.borderWidth = '1px'
     }
 
+    searchInput.style.borderBottomLeftRadius = '0';
+    searchInput.style.borderBottomRightRadius = '0';
+    autocompleteList.style.borderWidth = '1px';
 
     fetch(`/api/search-autocomplete/?term=${input}`)
       .then(response => response.json())
@@ -21,15 +33,19 @@ if (document.getElementById('searchInput')) {
         autocompleteList.innerHTML = '';
         data.forEach(item => {
           const div = document.createElement('div');
-          div.innerHTML = item;
+          div.textContent = item;
           div.addEventListener('click', function () {
-            document.getElementById('searchInput').value = this.innerText;
+            searchInput.value = this.textContent;
             autocompleteList.innerHTML = '';
-            document.getElementById('searchForm').submit();
+            searchForm.submit();
           });
           autocompleteList.appendChild(div);
         });
       })
-      .catch(error => console.log('Error:', error));
+      .catch(error => console.error('Error:', error));
+  }, 250);
+
+  searchInput.addEventListener('input', function () {
+    fetchAutocompleteData(this.value);
   });
-}
+});
