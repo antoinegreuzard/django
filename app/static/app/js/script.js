c009f402b8e98bcbb1eb9cc5791d3c1244c8441a
antoinegreuzard <antoine@antoinegreuzard.fr>
2024-03-01 11:29:36 +0100

    fix: search only on homepage

--- app/static/app/js/script.js
+++ app/static/app/js/script.js
@@ -1,33 +1,35 @@
-document.getElementById('searchInput').addEventListener('input', function () {
-  const input = this.value;
-  const autocompleteList = document.getElementById('autocomplete-list');
-  if (!input) {
-    document.getElementById('autocomplete-list').innerHTML = '';
-    this.style.borderBottomLeftRadius = ''
-    this.style.borderBottomRightRadius = ''
-    autocompleteList.style.borderWidth = ''
-    return;
-  } else {
-    this.style.borderBottomLeftRadius = '0'
-    this.style.borderBottomRightRadius = '0'
-    autocompleteList.style.borderWidth = '1px'
-  }
+if (document.getElementById('searchInput')) {
+  document.getElementById('searchInput').addEventListener('input', function () {
+    const input = this.value;
+    const autocompleteList = document.getElementById('autocomplete-list');
+    if (!input) {
+      document.getElementById('autocomplete-list').innerHTML = '';
+      this.style.borderBottomLeftRadius = ''
+      this.style.borderBottomRightRadius = ''
+      autocompleteList.style.borderWidth = ''
+      return;
+    } else {
+      this.style.borderBottomLeftRadius = '0'
+      this.style.borderBottomRightRadius = '0'
+      autocompleteList.style.borderWidth = '1px'
+    }
 
 
-  fetch(`/api/search-autocomplete/?term=${input}`)
-    .then(response => response.json())
-    .then(data => {
-      autocompleteList.innerHTML = '';
-      data.forEach(item => {
-        const div = document.createElement('div');
-        div.innerHTML = item;
-        div.addEventListener('click', function () {
-          document.getElementById('searchInput').value = this.innerText;
-          autocompleteList.innerHTML = '';
-          document.getElementById('searchForm').submit();
+    fetch(`/api/search-autocomplete/?term=${input}`)
+      .then(response => response.json())
+      .then(data => {
+        autocompleteList.innerHTML = '';
+        data.forEach(item => {
+          const div = document.createElement('div');
+          div.innerHTML = item;
+          div.addEventListener('click', function () {
+            document.getElementById('searchInput').value = this.innerText;
+            autocompleteList.innerHTML = '';
+            document.getElementById('searchForm').submit();
+          });
+          autocompleteList.appendChild(div);
         });
-        autocompleteList.appendChild(div);
-      });
-    })
-    .catch(error => console.log('Error:', error));
-});
+      })
+      .catch(error => console.log('Error:', error));
+  });
+}
