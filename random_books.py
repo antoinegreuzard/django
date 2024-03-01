a3b733b1d709db4923aa967261bbaa9c09aa747e
antoinegreuzard <antoine@antoinegreuzard.fr>
2024-03-01 10:09:07 +0100

    feat: add script to add random books

--- random_books.py
+++ random_books.py
@@ -1,14 +1,23 @@
-import os
-import django
-import datetime
+"""
+This script adds 20 books to the database using the Book model.
+It is designed to be run in a Django environment.
+"""
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
+import os
+import datetime
+import django
+
+# Configuring Django to execute commands outside of a server environment
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
 django.setup()
 
 from app.models import Book
 
 
 def add_books():
+    """
+    Creates and adds 20 books to the database with generic data.
+    """
     for i in range(1, 21):
         book = Book.objects.create(
             title=f'Book Title {i}',
