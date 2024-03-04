3eec3f960842b001f0afd44fc17bcbe3084a3410
antoinegreuzard <antoine@antoinegreuzard.fr>
2024-03-04 13:01:45 +0100

    refactor: better ui and ux

--- app/forms.py
+++ app/forms.py
@@ -2,7 +2,9 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
-from django.forms import NumberInput, DateInput, CheckboxSelectMultiple
+from django.forms import NumberInput, DateInput, CheckboxSelectMultiple, \
+    CheckboxInput, FileInput, RadioSelect, EmailField, CharField, \
+    PasswordInput, ModelMultipleChoiceField
 from django.contrib.auth import get_user_model
 
 from app.models import Book, Category
@@ -10,21 +12,42 @@ from app.models import Book, Category
 User = get_user_model()
 
 
-class UserLoginForm(forms.Form):
+class CustomFormMixin:
+    """Mixin to add 'form-control' class to all fields in a form, excluding
+    certain types of fields."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        exclude_widgets = (
+            RadioSelect,
+            CheckboxSelectMultiple,
+            CheckboxInput,
+            FileInput
+        )
+        for field_name, field in self.fields.items():
+            # Vérifie si le widget du champ actuel est dans la liste des
+            # widgets à exclure
+            if not isinstance(field.widget, exclude_widgets):
+                css_class = field.widget.attrs.get('class', '')
+                field.widget.attrs[
+                    'class'] = f'{css_class} form-control'.strip()
+
+
+class UserLoginForm(CustomFormMixin, forms.Form):
     """Form for handling user login."""
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    email = EmailField()
+    password = CharField(widget=PasswordInput())
 
 
-class UserRegisterForm(UserCreationForm):
+class UserRegisterForm(CustomFormMixin, UserCreationForm):
     """
     Form for handling new user registration.
 
     Extends Django's UserCreationForm to include email field and custom
     validation.
     """
-    email = forms.EmailField(required=True,
-                             help_text="Required. Add a valid email address.")
+    email = EmailField(required=True,
+                       help_text="Required. Add a valid email address.")
 
     class Meta(UserCreationForm.Meta):
         model = User
@@ -46,7 +69,7 @@ class UserRegisterForm(UserCreationForm):
         return user
 
 
-class BookForm(forms.ModelForm):
+class BookForm(CustomFormMixin, forms.ModelForm):
     """
     Form for creating and updating Book instances.
 
@@ -54,7 +77,7 @@ class BookForm(forms.ModelForm):
     picker for the 'date' field, and number inputs for 'price' and 'rate'
     fields to ensure proper data formatting.
     """
-    categories = forms.ModelMultipleChoiceField(
+    categories = ModelMultipleChoiceField(
         queryset=Category.objects.all(),
         widget=CheckboxSelectMultiple,
         required=False)
