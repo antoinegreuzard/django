44cedcb43a5fe5ace4bb59ad37978522454d665e
antoinegreuzard <antoine@antoinegreuzard.fr>
2024-03-06 12:47:29 +0100

    fix: disable debug

--- server/settings.py
+++ server/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7*-dvsvodh&le8uo+o=5^535m&ts)b9)!)(=wz-4fv9pv+d19f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '127.0.0.1:8000', 'localhost', 'localhost:8000',
                  'django-ne27.onrender.com']
