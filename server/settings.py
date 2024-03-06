3661a32b0ad0914923e025aa47cf9f30db456212
antoinegreuzard <antoine@antoinegreuzard.fr>
2024-03-06 16:40:42 +0100

    fix: js redirect

--- server/settings.py
+++ server/settings.py
@@ -85,6 +85,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
+    "/var/www/static/",
 ]
 
 # Database
@@ -131,7 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
